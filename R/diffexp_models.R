001475c72f35a9a13988ccb524505b3453438ad5
Combiz Khozoie <c.khozoie@imperial.ac.uk>
2019-11-15 21:34:34 +0000

    more de

--- R/diffexp_models.R
+++ R/diffexp_models.R
@@ -1,3 +1,24 @@
+################################################################################
+#' Perform Differential Gene Expression on a SingleCellExperiment
+#'
+#' @param sce a SingleCellExperiment object
+#' @param min_counts minimum number of counts
+#' @param min_cells_pc percentage of cells with min_counts for gene selection
+#' @param rescale_numerics rescaling numerics may improve model
+#' @param dependent_var the name of the colData variable for contrasts
+#' @param ref_class the class of dependent_var used as reference
+#' @param confounding_vars the independent variables of the model
+#' @param random_effects_var variable(s) to model as random effects
+#' @param fc_threshold fold change up/down threshold
+#' @param pval_cutoff the adjusted pvalue cutoff threshold
+#'
+#' @return results_l a list of DE table results
+#'
+#' @family differential gene expression
+#'
+#' @importFrom cli cli_text
+#'
+#' @export
 perform_de <- function(sce,
                        de_method = "MASTZLM",
                        min_counts = 1,
@@ -11,9 +32,7 @@ perform_de <- function(sce,
                        pval_cutoff = 0.05
                        ) {
 
-
-
-  sce <- .preprocess_sce_for_de(
+  pp_sce <- .preprocess_sce_for_de(
     sce = sce,
     min_counts = min_counts,
     min_cells_pc = min_cells_pc,
@@ -22,18 +41,27 @@ perform_de <- function(sce,
     ref_class = ref_class
   )
 
+  print(sce$cngeneson)
+
   if(de_method == "MASTZLM") {
     de_fn <- .perform_de_with_mast
     de_results <- do.call(
       de_fn,
       list(
-        sce = sce,
+        sce = pp_sce,
+        fc_threshold = fc_threshold,
+        pval_cutoff = pval_cutoff,
+        dependent_var = dependent_var,
+        ref_class = ref_class,
+        confounding_vars = confounding_vars,
+        random_effects_var = random_effects_var,
         fc_threshold = fc_threshold,
         pval_cutoff = pval_cutoff
       )
     )
   }
-  dependent_var_ref_class
+
+  return(de_results)
 }
 
 
@@ -53,6 +81,7 @@ perform_de <- function(sce,
 #' @importFrom SingleCellExperiment counts
 #' @importFrom SummarizedExperiment rowData colData
 #' @importFrom scater normalize
+#' @importFrom cli cli_text
 #'
 #' @keywords internal
 .preprocess_sce_for_de <- function(...) {
@@ -77,7 +106,12 @@ perform_de <- function(sce,
 
   # from MAST tutorial
   cdr2 <- colSums(SingleCellExperiment::counts(sce) > 0)
-  SummarizedExperiment::colData(sce)$cngeneson <- as.numeric(scale(cdr2))
+  sce$cngeneson <- as.numeric(scale(cdr2))
+
+  cli::cli_text(c(
+    "Setting '{.emph {args$ref_class}}' ",
+    "as the reference class of '{.strong {args$dependent_var}}'.")
+  )
 
   # define the reference class
   sce[[args$dependent_var]] <- relevel(
@@ -91,8 +125,7 @@ perform_de <- function(sce,
 
 
 ################################################################################
-#' Perform DE with MAST
-#'
+#' Perform DE with MAST#'
 #'
 #' @param sce a SingleCellExperiment object
 #'
@@ -103,66 +136,50 @@ perform_de <- function(sce,
 #' @importFrom SingleCellExperiment counts
 #' @importFrom SummarizedExperiment rowData colData
 #' @importFrom scater normalize
+#' @importFrom methods as
+#' @importFrom magrittr %>%
+#' @import MAST
 #'
 #' @keywords internal
 .perform_de_with_mast <- function(...) {
 
+  args <- list(...)
+
   message("##### Starting MAST Zero-Inflated Linear Mixed Model DE")
-
-
 
   sca <- as(sce, "SingleCellAssay")
 
-  # setup model variables
-  de_vars_l <- list()
-  de_vars_l[["model"]] <- list()
-  # de model variables
-  de_vars_l$model$dependent_var <- "group"
-  de_vars_l$model$confounding_vars <- c(
-    "sex",
-    "age", "pc_mito", "PMI", "total_features_by_counts", "seqdate"
-  )
-
-  de_vars_l$model$random_effects_vars <- "individual"
-  # within dependent group reference variable
-  de_vars_l$dep_var_ref_class <- "Control"
-
-  # add prefixes for estimable vars
-  de_vars_l$model_prefixed <- map(
-    de_vars_l$model, function(vars) {
-      .add_prefix_to_model_vars(vars, "sce$")
-    }
-  )
-
-  sce$group <- relevel(sce$group, ref = de_vars_l$dep_var_ref_class)
-
-  model_formula <- as.formula("~ group + individual + cngeneson + sex + age + PMI + RIN + seqdate + pc_mito")  #for bayesglm
+  message("Generating model formula")
+  model_formula <- do.call(.generate_model_from_vars, args)
 
   # fit model
   message("Fitting model\n")
+  print(sce$cngeneson)
   #options(warn=-1) #temporary silencing
   x <- Sys.time()
-  zlmCond <- zlm(
+  zlmCond <- MAST::zlm(
     formula = model_formula,
     sca = sca,
     method = "bayesglm", # note: glmer requires a random effects var
     ebayes = FALSE,
     parallel = TRUE
   )
+  message(Sys.time() - x)
 
   gc()
 
   ## test each contrast separately
   # obtain the group names for non-controls
-  dependent_var_names <- unique(sce[[de_vars_l$model$dependent_var]])
+  dependent_var_names <- unique(sce[[args$dependent_var]])
 
   dependent_var_names <- droplevels(
-    dependent_var_names[dependent_var_names != de_vars_l$dep_var_ref_class]
+    dependent_var_names[dependent_var_names != args$ref_class]
   )
 
-  contrasts <- map_chr(dependent_var_names, ~ paste0(de_vars_l$model$dependent_var, .))
-
-  # contrasts <- .add_prefix_to_model_vars(contrasts, "sce$")
+  contrasts <- purrr::map_chr(
+    dependent_var_names,
+    ~ paste0(args$dependent_var, .)
+    )
 
   results_l <- list()
 
@@ -170,22 +187,22 @@ perform_de <- function(sce,
     message(sprintf("Running DE for %s\n", ctrast))
 
     # only test the condition coefficient.
-    summaryCond <- summary(zlmCond, doLRT = ctrast)
+    summaryCond <- suppressWarnings(summary(zlmCond, doLRT = ctrast))
 
     # create data table of results
     summaryDt <- summaryCond$datatable
 
     # primerid is numbered for H, so retrieve rowname (ensembl)
-    row_num2name <- seq_along(rownames(counts(sca)))
-    names(row_num2name) <- rownames(counts(sca))
+    row_num2name <- seq_along(rownames(SingleCellExperiment::counts(sca)))
+    names(row_num2name) <- rownames(SingleCellExperiment::counts(sca))
 
     hurdle_pvals <- summaryDt %>%
       dplyr::filter(contrast == ctrast & component == "H") %>%
       dplyr::select(primerid, `Pr(>Chisq)`)
 
-    hurdle_pvals$primerid <- map_chr(
+    hurdle_pvals$primerid <- purrr::map_chr(
       hurdle_pvals$primerid,
-      function(x) names(row_num2name[as.numeric(x)])
+      ~ names(row_num2name[as.numeric(.)])
     )
 
     logFCcoefs <- summaryDt %>%
@@ -205,10 +222,7 @@ perform_de <- function(sce,
     )
 
     # append gene names
-    ensembl_res <- EnsemblGeneVersionVectorToGeneNameVector(
-      fcHurdle$ensembl_gene_id,
-      mappings_file = ensembl_mappings
-    ) %>%
+    ensembl_res <- map_ensembl_gene_id(fcHurdle$ensembl_gene_id) %>%
       dplyr::rename(gene = external_gene_name)
 
     fcHurdle <- merge(fcHurdle, ensembl_res, by = "ensembl_gene_id")
@@ -216,25 +230,28 @@ perform_de <- function(sce,
     fcHurdle$padj <- p.adjust(fcHurdle$pval, "fdr", n = dim(sce)[[1]])
 
     fcHurdle$contrast <- ctrast
+    fcHurdle$reference <- args$ref_class
 
     model_formula_string <- as.character(Reduce(paste, deparse(model_formula)))
     fcHurdle$model <- gsub(" ", "", model_formula_string, fixed = TRUE) # no whitespace
 
-    results_l[[ctrast]] <- fcHurdle
+    results <- fcHurdle %>%
+      dplyr::filter(padj <= args$pval_cutoff) %>%
+      dplyr::filter(gene_biotype == "protein_coding") %>%
+      dplyr::filter(abs(logFC) > log2(args$fc_threshold)) %>%
+      dplyr::arrange(-logFC)
+
+    element_name <- paste(args$ref_class, ctrast, sep = "_vs_")
+    results_l[[element_name]] <- results
   }
 
+  merged_name <- paste0(args$ref_class, "_Merged_Results")
   message("Merging results")
-  results <- Reduce(rbind, results_l)
-
-  results <- results %>%
-    dplyr::filter(padj <= pval_cutoff) %>%
-    dplyr::filter(gene_biotype == "protein_coding") %>%
-    dplyr::filter(abs(logFC) > log2(fc_threshold)) %>%
-    dplyr::arrange(-logFC)
+  results_l[[merged_name]] <- Reduce(rbind, results_l)
 
   message("Done!  Returning results")
 
-  return(results)
+  return(results_l)
 }
 
 
@@ -277,5 +294,56 @@ perform_de <- function(sce,
   ))
 
   return(sce)
+
+}
+
+
+
+################################################################################
+#' Generate a model formula from dependent, confounding, and random vars
+#'
+#' @param dependent_var the dependent variable
+#' @param confounding_vars the confounding variables
+#' @param re_vars the random effect variables (optional)
+#'
+#' @return model_formula a model formula
+#'
+#' @family differential gene expression
+#'
+#' @importFrom purrr map_chr
+#'
+#' @keywords internal
+.generate_model_from_vars <- function(...) {
+
+  args <- list(...)
+
+  if (!is.null(args$random_effects_var)) {
+    message("Generating random effects model")
+    args$random_effects_var <- purrr::map_chr(
+      args$random_effects_var,
+      function(var) sprintf("(1|%s)", var)
+    )
+    args$random_effects_var <- paste(args$random_effects_var, collapse = " + ")
+
+    model_formula <- as.formula(
+      sprintf(
+        "~ %s + %s + %s",
+        args$dependent_var,
+        args$random_effects_var,
+        paste(args$confounding_vars, collapse = " + ")
+      )
+    )
+  } else {
+    message("Generating mixed effects model")
+    model_formula <- as.formula(
+      sprintf(
+        "~ %s + %s",
+        args$dependent_var,
+        paste(args$confounding_vars, collapse = " + ")
+      )
+    )
+  }
+
+  return(model_formula)
 
 }
