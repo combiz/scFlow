9a24c36e2932576bcb52241478cb09ee84c78b23
Mahdi Moradi Marjaneh <mmoradim@login-6.rcs.ic.ac.uk>
2020-01-29 14:44:05 +0000

    Updated Liger and reduce_dims functions, Reduced Liger parameters

---
+++ R/liger_preprocess.R
@@ -0,0 +1,94 @@
+################################################################################
+#' Preprocessing steps for Liger dimensionality reduction  
+#'
+#'
+#' Split merged object into multiple sce objects and extract sparse matrices:
+#' @param sce_merged merged SingleCellExperiment objects
+#'
+#' Make a Liger object:
+#' @param raw.data List of expression matrices (gene by cell) each from a SingleCellExperiment object.
+#' @param take.gene.union Whether to fill out raw.data matrices with union of genes across all
+#'   datasets (filling in 0 for missing data) (requires make.sparse=T) (default FALSE).
+#' @param remove.missing Whether to remove cells not expressing any measured genes, and genes not
+#'   expressed in any cells (if take.gene.union = T, removes only genes not expressed in any
+#'   dataset) (default TRUE).
+#'
+#'  Select informative genes:
+#' @param num.genes Number of genes to find for each dataset. Set to 3000 as default.
+#' @param combine How to combine variable genes across experiments. Either "union" or "intersect".
+#'   (default "union")
+#' @param keep.unique Keep genes that occur (i.e., there is a corresponding column in raw.data) only
+#'    in one dataset (default FALSE)
+#' @param capitalize Capitalize gene names to match homologous genes (ie. across species)
+#'   (default FALSE)
+#' @param do.plot Display log plot of gene variance vs. gene expression for each dataset.
+#'   Selected genes are plotted in green. (default FALSE)
+#' @param cex.use Point size for plot.
+#'
+#'  Scale genes by root-mean-square across cells:
+#' @param remove.missing Whether to remove cells from scale.data with no gene expression
+#'   (default TRUE)
+#'
+#' Remove cells/genes with no expression across any genes/cells:
+#' @param use.cols Treat each column as a cell (default TRUE)
+#'
+#' @return liger preprocessed object.
+#'
+#' @importFrom liger createLiger 
+#' @importFrom liger normalize
+#' @importFrom liger selectGenes
+#' @importFrom liger scaleNotCenter
+#' @importFrom liger removeMissingObs
+#'
+#' @export
+
+liger_reduce_dims <- function(sce_merged,
+							take.gene.union = F, 
+                            remove.missing = T,
+							num.genes = 3000,
+							combine = "union",
+							keep.unique = F, 
+							capitalize = F, 
+							do.plot = F, 
+							cex.use = 0.3
+							remove.missing = T,
+							use.cols = T,
+                            ...) {
+
+  sce_merged@metadata$liger_params <- list()
+
+  # Split merged sce object into multiple sce objects and extract sparse matrices
+  datasetList <- list()
+  matList <- list()
+  manifests <- unique(sce_merged@colData$manifest)
+  for(mnft in manifests){
+		datasetName <- paste0("dataset_", mnft)
+		datasetList[[datasetName]] <- sce_merged[,sce_merged@colData$manifest == mnft]
+		matList[[datasetName]] <- sce_merged@assays$data$counts[,sce_merged@colData$manifest == mnft]
+  }
+  
+  # Make a Liger object. Pass in the sparse matrix.  
+  ligerex <- createLiger(raw.data = matList, take.gene.union = take.gene.union, 
+                         remove.missing = remove.missing)
+
+  ### preprocessing steps
+  
+  # Normalize the data to control for different numbers of UMIs per cell
+  ligerex <- liger::normalize(ligerex)
+
+  # Select variable (informative) genes 
+  
+  ligerex <- liger::selectGenes(ligerex, num.genes = num.genes, combine = combine, keep.unique = keep.unique, 
+                         capitalize = capitalize, do.plot = do.plot, cex.use = cex.use)
+  
+  sce_merged@metadata$liger_params$liger_selectedGenes <- 3000
+
+  # Scale the data by root-mean-square across cells
+  ligerex = liger::scaleNotCenter(ligerex, remove.missing = remove.missing)
+
+  # Remove cells/genes with no expression across any genes/cells
+  ligerex <- liger::removeMissingObs(ligerex, use.cols = use.cols)
+  
+  return(ligerex)
+
+}
